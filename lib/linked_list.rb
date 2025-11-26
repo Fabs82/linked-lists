01ef4c74781e652e6d28015d9882e57b93b7da7b
Fabs82 <148846635+Fabs82@users.noreply.github.com>
2025-11-26 17:03:53 +0100

    feat: add methods to the LinkedList class
    * append(value) adds a new node containing value to the end of the list
    * prepend(value) adds a new node containing value to the start of the list
    * size returns the total number of nodes in the list
    * head returns the first node in the list
    * tail returns the last node in the list
    * at(index) returns the node at the given index
    * pop removes the last element from the list
    * contains?(value) returns true if the passed in value is in the list and otherwise returns false.
    * find(value) returns the index of the node containing value, or nil if not found.
    * to_s represent your LinkedList objects as strings, so you can print them out and preview them in the console.
    * insert_at(value, index) that inserts a new node with the provided value at the given index.
    * remove_at(index) that removes the node at the given index.

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -18,17 +18,34 @@ class LinkedList
       @head = new_node
     else
       # if NOT empty assugn HEAD to a variable
-      current_value = @head
-      # Traverse the list with a loop updateing the current_value (starting from HEAD) to the next_node until it points to nil
+      current_node = @head
+      # Traverse the list with a loop updateing the current_node (starting from HEAD) to the next_node until it points to nil
       # Rhen next_node is NIL list has been traversed completely and the loop ends
-      current_value = current_value.next_node until current_value.next_node.nil?
+      current_node = current_node.next_node until current_node.next_node.nil?
       # Add the new node at the end of the list
-      current_value.next_node = Node.new(value)
+      current_node.next_node = Node.new(value)
       # Update the TAIL variable (DEBUG to allow append and append_tail to be used together)
-      @tail = current_value.next_node
+      @tail = current_node.next_node
     end
     # Increase the counter for the list size
     @size += 1
+  end
+
+  def pop
+    # The method removes the last element of the list
+    if @head.nil?
+      nil
+    elsif @head.next_node.nil?
+      @head = nil
+      @tail = nil
+      @size = 0
+    else
+      current_node = @head
+      current_node = current_node.next_node until current_node.next_node.next_node.nil?
+      current_node.next_node = nil
+      @tail = current_node
+      @size -= 1
+    end
   end
 
   def append_tail(value)
@@ -59,9 +76,9 @@ class LinkedList
     if @head.next_node.nil?
       @head
     else
-      current_value = @head
-      current_value = current_value.next_node until current_value.next_node.nil?
-      current_value
+      current_node = @head
+      current_node = current_node.next_node until current_node.next_node.nil?
+      current_node
     end
   end
 
@@ -72,13 +89,110 @@ class LinkedList
     @head
   end
 
+  def at(index)
+    # The method returns the node in a given position
+    if @size.zero?
+      puts "List is empty"
+      nil
+    elsif index >= @size
+      puts "Search exceeds list size"
+      nil
+    else
+      count = 0
+      current_node = @head
+      while count < index
+        current_node = current_node.next_node
+        count += 1
+      end
+      current_node
+    end
+  end
+
+  def contains?(value)
+    # The method search the list for a certain given value. Returns TRUE if found or FALSE if not found
+    current_node = @head
+    # traverse the list
+    while current_node
+      # check if node.value == value
+      if current_node.value == value
+        # if it is return true
+        puts "Found"
+        return true
+      end
+      # if the current_node is not the value and there are still elementsin the list, move to the next node
+      current_node = current_node.next_node
+    end
+    # if not found return false
+    puts "Not found"
+    false
+  end
+
+  def find_index(value)
+    # The method returns the index of the node containing the value or nil if not found
+
+    # set a counter to 0
+    counter = 0
+    current_node = @head
+    # traverse the list
+    while current_node
+      # check if the node.value == value and if it is return the counter and end the loop
+      return counter if current_node.value == value
+
+      # else advance in the list and increase counter
+      current_node = current_node.next_node
+      counter += 1
+    end
+    # return nil if loop ends without finding the value
+    nil
+  end
+
+  def insert_at(value, index)
+    # The method inserts a new node with the provided value att the given index
+    return prepend(value) if index.zero?
+
+    counter = 0
+    current_node = @head
+    while current_node
+      if counter == index - 1
+        puts "insert - #{value} - at position #{index}"
+        @size += 1
+        return current_node.next_node = Node.new(value, current_node.next_node)
+      end
+      current_node = current_node.next_node
+      counter += 1
+    end
+    puts "Error: Index #{index} is out of bounds for insertion."
+  end
+
+  def remove_at(index)
+    # The method removes the node at the given index
+    if index.zero?
+      @head = @head.next_node
+      @size -= 1
+      return
+    end
+
+    counter = 0
+    current_node = @head
+
+    while current_node
+      if counter == index - 1
+        @size -= 1
+        return current_node.next_node = current_node.next_node.next_node
+      end
+      current_node = current_node.next_node
+      counter += 1
+    end
+    puts "Error: Index #{index} is out of bounds for deletion."
+  end
+
   def to_s
     # The method prints out the value of the list nodes in the form of a string
     string = ""
-    current_value = @head
-    while current_value
-      string += "( #{current_value.value} ) -> "
-      current_value = current_value.next_node
+    current_node = @head
+    while current_node
+      string += "( #{current_node.value} ) -> "
+      current_node = current_node.next_node
     end
     string + " nil "
   end
--- main.rb
+++ main.rb
@@ -1,11 +1,37 @@
 require_relative "./lib/linked_list"
 
 list = LinkedList.new
-list.append("HEAD")
+# adds a new Node at the end traversing the entire list
 list.append("cat")
+# adds a new Node at the start of the list
+list.prepend("HEAD")
+# returns the first node in the list
+puts list.head.value
 list.append("fox")
 list.append("deer")
 list.append("TAIL")
+# returns the last node in the list
+puts list.tail.value
+# adds a new Node directly at the end of the list
 list.append_tail("TAIL 2")
-
+# returns the number of elements in the list
+puts list.size
+# removes the last element of the list
+list.pop
+puts list.size
+list.append_tail("TAIL 3")
+puts list.size
+# returns the node in a given position of the list
+puts list.at(1)
+# returns TRUE if value is in the list else FALSE
+list.contains?("TAIL")
+# returns the index of teh value if in the list else NIL
+p list.find_index("deer")
+# inserts the Node at a specific index
+list.insert_at("bobcat", 0)
 puts list
+puts list.size
+# removes the Node at a specific index
+list.remove_at(0)
+puts list
+puts list.size
